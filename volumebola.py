789fa81b0d2f2496e8f09a5082eeedf31cc58a70
altivanie <163065885+altivanie@users.noreply.github.com>
2024-03-11 15:51:37 -0700

    Add files via upload

---
+++ volumebola.py
@@ -0,0 +1,12 @@
+print('## Program Menghitung Volume Bola ##')
+print ('====================================================')
+Nama = 'Alti Vanie Diandra Eka Putri'
+print('Nama:', Nama)
+Nim = '2310432002'
+print('Nim:', Nim)
+Kelas = 'B'
+print('Kelas:', Kelas)
+print ('====================================================')
+jari_jari = float(input("Jari-jari bola="))
+volume = (4/3) * 3.14 * (jari_jari * 3)
+print("Volume bola dengan jari-jari", jari_jari, "adalah:", volume)
