7c4a6c71eb3ddf6ccb81aac6b820576e5a8d4113
altivanie <163065885+altivanie@users.noreply.github.com>
2024-03-11 15:48:31 -0700

    Add files via upload

---
+++ perkaliansederhana.py
@@ -0,0 +1,11 @@
+print('## Program Perkalian Sederhana ##')
+Nama = 'Alti Vanie Diandra Eka Putri'
+print('Nama:', Nama)
+Nim = '2310432002'
+print('Nim:', Nim)
+Kelas = 'B'
+print('Kelas:', Kelas)
+angka1 = float(input("angka 1="))
+angka2 = float(input("angka 2="))
+hasil = angka1 * angka2
+print("Hasil perkalian:", hasil)
