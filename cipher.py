ceebdcb42701cbc42694eec8d73e8ef6d1e7a01d
altivanie <163065885+altivanie@users.noreply.github.com>
2024-04-15 01:06:50 -0700

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -10,7 +10,7 @@ print('====================================================')
 
 print('Selamat Datang di Caesar Cipher Program')
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet = alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 cipher = ""
 
 teks = input('Masukkan teks: ').lower()
