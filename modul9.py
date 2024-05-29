b7e372f14f2e55731da41342b2961f15de89426e
altivanie <163065885+altivanie@users.noreply.github.com>
2024-05-28 18:24:39 -0700

    Add files via upload

---
+++ modul9.py
@@ -0,0 +1,22 @@
+import os
+from termcolor import colored, cprint
+os.system
+
+Nama = 'Alti Vanie Diandra Eka Putri'
+print('Nama:', Nama)
+Nim = '2310432002'
+print('Nim:', Nim)
+Kelas = 'B'
+print('Kelas:', Kelas)
+print('=====================================================')
+cprint('Berikut Ini adalah bendera Indonesia','yellow')
+print()
+
+for i in range(4):
+    cprint(' ' * 25, 'red', 'on_red')
+
+for i in range(4):
+    cprint(' ' * 25, 'white', 'on_white')
+
+
+
